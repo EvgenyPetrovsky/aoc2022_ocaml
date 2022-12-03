1b84df5121acb021d36a77ae607ce586eafd30df
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2022-12-03 19:18:43 +0100

    solve day 1 and day 2

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,20 @@
-let () = print_endline "Hello, World!"
+open Stdio
+
+(* this main function reads sandard input with puzzle data from file and returns standard output with answer *)
+let () =
+  (*let solver = Day_01 in*)
+  let input_text = In_channel.input_all Stdio.stdin in
+  let answer1_text = input_text
+    |> Day_02.text_to_input
+    |> Day_02.part1
+    |> Day_02.answer_to_text
+  in
+  let answer2_text = input_text
+    |> Day_02.text_to_input
+    |> Day_02.part2
+    |> Day_02.answer_to_text
+  in
+  printf
+    "Part 1 solution is: %s;\nPart 2 solution is: %s.\n"
+    answer1_text answer2_text
+
--- bin/solution.ml
+++ bin/solution.ml
@@ -2,22 +2,60 @@ open Base
 
 module type SOLUTION =
   sig
-    type text = string list
+    type text = string
     type input
     val text_to_input : text -> input
     type answer
     type solution = input -> answer
     type part1 = solution
     type part2 = solution
-    val solution_to_text : answer -> string
+    val answer_to_text : answer -> string
   end
 
 module Day_01  = struct
-  type input = int list
-  type answer = int
+  type input = Input of int list list
+  type answer = Answer of int | Unknown
 
-  let text_test =
-    [ "1000"; "2000"; "3000"; ""; "4000"; ""; "5000"; "6000"; ""; "7000"; "8000"; "9000"; ""; "10000"]
+  let text_test =  [ "1000" ^ "\n" ^ "2000" ^ "\n" ^ "3000" ^ "\n" ^ "" ^ "\n" ^ "4000" ^ "\n" ^ "" ^ "\n" ^ "5000" ^ "\n" ^ "6000" ^ "\n" ^ "" ^ "\n" ^ "7000" ^ "\n" ^ "8000" ^ "\n" ^ "9000" ^ "\n" ^ "" ^ "\n" ^ "10000"]
 
+  (* function that extracts integer number from the string *)
+  let extract_int = function
+    | "" -> None
+    | t  -> Some (Int.of_string t)
+
+  let text_to_input (t: string) :input = t
+    (* Split raw text into lines first*)
+    |> String.split_lines
+    (* Some lines are empty and we use it as a separator to create list of lists*)
+    |> List.group ~break:(fun x _ -> String.(x = ""))
+    (* filter out empty element from nested lists and convert elements to int *)
+    |> List.map ~f:(fun x -> List.filter_map x ~f:extract_int)
+    |> (fun x -> Input x)
+
+  (* Solution for part 1 *)
+  let part1 (Input i : input) : answer = i
+    (* Sum elements of nested lists *)
+    |> List.map ~f:(List.fold ~init:0 ~f:((+)))
+    (* select biggest number *)
+    |> List.max_elt ~compare:(Int.compare)
+    (* biggest number is returned as optional type, use 0 as default when None *)
+    |> (fun x -> match x with | None -> Answer 0 | Some n -> Answer n)
+
+  (* Solution for part 2 *)
+  let part2 (Input i : input) : answer = i
+    (* Sum elements of nested lists *)
+    |> List.map ~f:(List.fold ~init:0 ~f:(+))
+    (* sort list descending *)
+    |> List.sort ~compare:(Int.compare)
+    |> List.rev
+    (* take biggest 3 elements - they go first after sorting *)
+    |> (fun x -> List.take x 3)
+    (* sum elements *)
+    |> List.fold ~init:0 ~f:(+)
+    |> (fun x -> Answer x)
+
+  let answer_to_text = function
+    | Answer x -> Int.to_string x
+    | Unknown  -> "Solution not yet implemented"
 
 end
