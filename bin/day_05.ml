9a44c0eed6a42b50cf1ef03aa87113729bd0239b
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2022-12-05 23:32:16 +0100

    day 5 solutions

--- bin/day_05.ml
+++ bin/day_05.ml
@@ -1,48 +1,90 @@
 open Base
 
 type proc = { from_: int ; to_: int ; num_: int }
-type stack = char list list
-type input = Input of { stacks: stack ; procs: proc list }
+type stack = char list
+type crane_model = CM9000 | CM9001
+type input = Input of { stacks: stack list ; procs: proc list }
 
 type answer = Answer of char list | Unknown
- 
-type t1 = char list array
 
-let grow_one_stack (lines : string list) (position: int) : char list =
-  lines
-  |> List.map ~f:(fun x -> String.get x position)
+let read_crate_by_stack_num (text_line:string) (stack_num:int) : char =
+  (* position of first stack is in character with index 1 (starts with 0) *)
+  let pos = 4 * stack_num - 3 in
+  if pos > String.length text_line then ' ' else String.get text_line pos
 
-let parse_stacks (lines : string list) : stack = 
+(* stacks are defined as few lines in the beginning of file
+   in the bottom of stacks are the numbers 1 for first, 2 for second, etc
+   stacks are going to be represented as lists *)
+let parse_stacks (lines : string list) : (stack list)=
   (* first let's find that line with numbers of stacks - it is last line before empty line *)
   let line_with_nums = List.hd_exn @@ List.drop (List.rev lines) 1 in
+  (* ... and  lines that contain crates *)
+  let lines_with_crates = List.rev @@ List.drop (List.rev lines) 2 in
   (* second - get list of numbers in it*)
-  let nums = line_with_nums
-  |> String.split ~on:' '
-  |> List.filter_map ~f:(function | "" -> None | x -> Some (Int.of_string x)) in
-  let positions = List.init (List.length nums) ~f:(fun x -> 1 + x * 3) |> List.rev in
-  
-  (* reverse lines so that stacks grow down *)
-  List.drop (List.rev lines) 2
-  (* drop 2 lines : empty line and the line with stack numbers *)
-  |> (fun lns -> List.map positions ~f:(fun pos -> grow_one_stack lns pos))
+  let stack_nums =
+    line_with_nums
+    |> String.split ~on:' '
+    |> List.filter_map ~f:(function | "" -> None | x -> Some (Int.of_string x))
+  in
+  stack_nums
+  |> List.map ~f:(fun s -> List.map lines_with_crates ~f:(fun l -> read_crate_by_stack_num l s))
+  |> List.map ~f:(fun s -> List.filter s ~f:(fun c -> Char.(<>) c ' '))
 
-let parse_procs (_ : string list) = [{ from_ = 1 ; to_ = 2 ; num_ = 3 }]
-let text_to_input (t: string) :input = 
+
+(* procedures are lines of format "move N from N to N" *)
+let parse_procs (procs : string list) =
+  let ios = Int.of_string in
+  let parse_line (line: string) : proc option =
+    match ( String.split ~on:' ' line ) with
+    | "move"::n::"from"::s::"to"::t::_ -> Some { from_ = ios s ; to_ = ios t ; num_ = ios n }
+    | _ -> None
+  in
+  procs
+  |> List.filter_map ~f:parse_line
+
+let text_to_input (t: string) :input =
   t
   |> String.split_lines
   |> List.group ~break:(fun x _ -> String.(x = ""))
-  |> (fun lst -> 
-       let stk = List.hd_exn lst in 
+  |> (fun lst ->
+       let stk = List.hd_exn lst in
        let prc = List.hd_exn @@ List.drop lst 1 in
        let stacks = parse_stacks stk in
        let procs  = parse_procs prc in
        Input {stacks ; procs})
 
-(* Solution for part 1 *)
-let part1 (Input _ : input) : answer = Unknown
+let rec restack_by_1 (from_ : stack) (to_ : stack ) (n : int) : (stack * stack) =
+  match (from_, to_, n) with
+  | ([], _, _) | (_, _, 0) -> (from_, to_)
+  | ((hd::tl), _, n) -> restack_by_1 tl (hd::to_) (n-1)
+
+let restack_by_n (from_ : stack) (to_ : stack ) (n : int) : (stack * stack) =
+  let (move, keep) = List.split_n from_ n in
+  (keep, List.append move to_)
+
+let move_crates (stacks: stack list) (proc: proc) (model: crane_model) : (stack list) =
+  let a = Array.of_list stacks in
+  let (from_, to_, n) = (proc.from_ - 1, proc.to_ - 1, proc.num_) in
+  let (s1, s2) = match model with
+  | CM9000 -> restack_by_1 (Array.get a from_) (Array.get a to_) n
+  | CM9001 -> restack_by_n (Array.get a from_) (Array.get a to_) n
+  in
+  Array.set a from_ s1; Array.set a to_ s2;
+  Array.to_list a
 
 (* Solution for part 1 *)
-let part2 (Input _ : input) : answer = Unknown
+let part1 (Input { stacks ; procs } : input) : answer =
+  procs
+  |> List.fold ~init:stacks ~f:(fun z x -> move_crates z x CM9000)
+  |> List.filter_map ~f:List.hd
+  |> (fun x -> Answer x)
+
+(* Solution for part 1 *)
+let part2 (Input { stacks ; procs } : input) : answer =
+procs
+|> List.fold ~init:stacks ~f:(fun z x -> move_crates z x CM9001)
+|> List.filter_map ~f:List.hd
+|> (fun x -> Answer x)
 
 let answer_to_text = function
   | Answer x -> String.of_char_list x
--- bin/main.ml
+++ bin/main.ml
@@ -5,14 +5,14 @@ let () =
   (*let solver = Day_01 in*)
   let input_text = In_channel.input_all Stdio.stdin in
   let answer1_text = input_text
-    |> Day_04.text_to_input
-    |> Day_04.part1
-    |> Day_04.answer_to_text
+    |> Day_05.text_to_input
+    |> Day_05.part1
+    |> Day_05.answer_to_text
   in
   let answer2_text = input_text
-    |> Day_04.text_to_input
-    |> Day_04.part2
-    |> Day_04.answer_to_text
+    |> Day_05.text_to_input
+    |> Day_05.part2
+    |> Day_05.answer_to_text
   in
   printf
     "Part 1 solution is: %s;\nPart 2 solution is: %s.\n"
