840fb7d918501376939ac81f5c66b64396eef502
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2022-12-06 08:33:32 +0100

    day 6 solutions

---
+++ bin/day_06.ml
@@ -0,0 +1,32 @@
+open Base
+
+type input = Input of char list
+type answer = Answer of int | Unknown
+type marker = Packet | Message
+
+let text_to_input (t: string) :input =
+  let chars = String.to_list t in
+  Input chars
+
+let identify_start_of (m:marker) (cs: char list) =
+  let len = match m with | Packet -> 4 | Message -> 14 in
+  let rec identify len cs acc =
+    let (beg, _) = List.split_n cs len in
+    let unq_len = List.dedup_and_sort ~compare:(Char.compare) beg in
+    if List.length unq_len = 0 then 0
+    else if List.length unq_len = len then acc + len
+    else identify len (List.drop cs 1) (acc + 1)
+  in
+  identify len cs 0
+
+(* Solution for part 1 *)
+let part1 (Input i : input) : answer =
+  Answer (identify_start_of Packet i)
+
+(* Solution for part 1 *)
+let part2 (Input i : input) : answer =
+  Answer (identify_start_of Message i)
+
+let answer_to_text = function
+  | Answer x -> Int.to_string x
+  | Unknown  -> "Solution not yet implemented"
--- bin/main.ml
+++ bin/main.ml
@@ -5,14 +5,14 @@ let () =
   (*let solver = Day_01 in*)
   let input_text = In_channel.input_all Stdio.stdin in
   let answer1_text = input_text
-    |> Day_05.text_to_input
-    |> Day_05.part1
-    |> Day_05.answer_to_text
+    |> Day_06.text_to_input
+    |> Day_06.part1
+    |> Day_06.answer_to_text
   in
   let answer2_text = input_text
-    |> Day_05.text_to_input
-    |> Day_05.part2
-    |> Day_05.answer_to_text
+    |> Day_06.text_to_input
+    |> Day_06.part2
+    |> Day_06.answer_to_text
   in
   printf
     "Part 1 solution is: %s;\nPart 2 solution is: %s.\n"
