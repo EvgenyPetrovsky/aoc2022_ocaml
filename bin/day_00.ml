e30091edc10f2d31031fc753e6cc527eb6a43fce
Evgeny Petrovsky <evgeny.petrovsky@ing.com>
2022-12-08 16:00:03 +0100

    add non_implemented exception

--- bin/day_00.ml
+++ bin/day_00.ml
@@ -1,11 +1,13 @@
 open Base
 
 type t = Empty
+exception Not_implemented
 
 type input = Input of t
 type answer = Answer of int | Unknown
 
-let text_to_input (t: string) :input = Input Empty
+let text_to_input (t: string) :input = 
+  raise Not_implemented
 
 (* Solution for part 1 *)
 let part1 (Input i : input) : answer = Unknown
