b704e1311fd4ba39c8c0a048779a1c0ff0503f60
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2022-12-20 22:45:15 +0100

    some peparations for day 17 and 19

---
+++ lib/day_19.ml
@@ -0,0 +1,32 @@
+open Base
+
+type t = Empty
+exception Not_implemented
+
+type input = Input of t
+type answer = Answer of int | Unknown
+
+
+(* Answer print function *)
+let answer_to_text = function
+  | Answer x -> Int.to_string x
+  | Unknown  -> "Solution not yet implemented"
+
+
+  (* Parse input functions *)
+let text_to_input (_: string) :input =
+  raise Not_implemented
+
+
+(* Debug functions *)
+
+
+(* Solution helper functions *)
+
+
+(* Solution for part 1 *)
+let part1 (Input _ : input) : answer = Unknown
+
+
+(* Solution for part 2 *)
+let part2 (Input _ : input) : answer = Unknown
