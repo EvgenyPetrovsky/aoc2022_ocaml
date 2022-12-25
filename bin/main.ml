81bb871a1a8222cbdbc033aa3e0a4dabaefcec89
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2022-12-25 16:17:35 +0100

    day 25 part 1

--- bin/main.ml
+++ bin/main.ml
@@ -21,8 +21,9 @@ module D20 = Aoc2022_ocaml.Day_20
 module D21 = Aoc2022_ocaml.Day_21
 module D22 = Aoc2022_ocaml.Day_22
 module D23 = Aoc2022_ocaml.Day_23
+module D25 = Aoc2022_ocaml.Day_25
 
-module Today = D23
+module Today = D25
 
 (* this main function reads sandard input with puzzle data from file and returns standard output with answer *)
 let () =
